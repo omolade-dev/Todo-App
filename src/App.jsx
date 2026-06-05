4d2ab27c7d270b0583941b7f09bbbc4a7f4f2d0b
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-05 14:41:41 +0100

    feat: restructure App component layout for improved organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,35 @@
 function App() {
   return (
-  <div>
-    <div className="header">
-      <h1>My Tasks</h1>
-
-      <p>1 of 3 completed</p>
-    </div>
-
     <div>
-      <input type="text" placeholder="Add a new task..." />
-      <button> + Add</button>
+      <div className="logo">
+        <div className="img">
+          <img src="" alt="" />
+        </div>
+        <div className="header">
+          <h1>My Tasks</h1>
+
+          <p>1 of 3 completed</p>
+        </div>
+      </div>
+
+      <div>
+        <input type="text" placeholder="Add a new task..." />
+        <button> + Add</button>
+      </div>
+
+      <div>
+        <h3>All</h3>
+        <h3>Pending</h3>
+        <h3>Completed</h3>
+      </div>
+
+      <div>
+        <p></p>
+        <p></p>
+        <p></p>
+      </div>
     </div>
-
-    
-  </div>
-
-  )
+  );
 }
 
 export default App;
