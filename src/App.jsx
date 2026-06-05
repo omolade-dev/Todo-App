059d649c1cd8bf7de1ca6facef4d6fd64602b442
omoladeajala <shadowcoderx.02@gmail.com>
2026-06-05 14:36:18 +0100

    feat: add initial setup for React application with Vite
    - Added React and Vite configuration files.
    - Created main entry point for the application (main.jsx).
    - Included global styles (index.css).
    - Added SVG assets for React and Vite logos.

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+  return (
+  <div>
+    <div className="header">
+      <h1>My Tasks</h1>
+
+      <p>1 of 3 completed</p>
+    </div>
+
+    <div>
+      <input type="text" placeholder="Add a new task..." />
+      <button> + Add</button>
+    </div>
+
+    
+  </div>
+
+  )
+}
+
+export default App;
